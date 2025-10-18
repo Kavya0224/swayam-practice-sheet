269c35ede4653b8727371645c6bfdd5018d2feb7
Kavya0224 <kavyasanger448@gmail.com>
2025-10-18 15:07:11 +0530

    Time: 59 ms (33.55%) | Memory: 220.5 MB (9.78%) - LeetSync

--- 817-design-hashmap/design-hashmap.cpp
+++ 817-design-hashmap/design-hashmap.cpp
@@ -3,21 +3,27 @@ using namespace std;
 
 class MyHashMap {
 public:
-    vector<int> data;
+    vector<int> data;  // Vector to store the values for keys, indexed by key
 
     MyHashMap() {
-        // Declare vector of size 100001 with all values initialized to -1
+        // Initialize the vector 'data' with size 1,000,001 (to cover keys 0 to 1,000,000)
+        // All values are set to -1, indicating that no key is currently mapped
         data = vector<int>(1000001, -1);
     }
 
+    // Insert a (key, value) pair into the hashmap.
+    // If the key already exists, update the value.
     void put(int key, int val) {
         data[key] = val;
     }
 
+    // Return the value to which the specified key is mapped,
+    // or -1 if this map contains no mapping for the key.
     int get(int key) {
         return data[key];
     }
 
+    // Remove the mapping for the specified key by setting its value to -1.
     void remove(int key) {
         data[key] = -1;
     }
