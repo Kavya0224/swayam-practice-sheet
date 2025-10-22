29ea0a23a8b61388ef1780b4ada331e1f570f32b
Kavya0224 <kavyasanger448@gmail.com>
2025-10-22 21:59:40 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (13.37%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,24 +1,12 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();       // Total number of elements in the input array (which is missing 1 number)
-        int sum = 0;               // Variable to store the sum of all elements in the input array
-        int expsum = 0;            // Variable to store the expected sum from 0 to n
-
-        // Calculate expected sum of numbers from 0 to n using loop
-        for (int i = 0; i <= n; i++) {
-            expsum += i;
-        }
-
-        // Calculate actual sum of elements present in the array
+        int n = nums.size();
+        int missing = 0;
         for (int i = 0; i < n; i++) {
-            sum += nums[i];
+            missing ^= i ^ nums[i];
         }
-
-        // The missing number is the difference between expected sum and actual sum
-        return expsum - sum;
+        missing ^= n; // This line is essential
+        return missing;
     }
 };
