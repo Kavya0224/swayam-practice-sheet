e98802cf471a2d76208a4f38a22c5e899711c4df
Kavya0224 <kavyasanger448@gmail.com>
2025-10-30 10:58:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (49.75%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> arr(n+1);
-        arr[0]=1;
-        arr[1]=1;
-        for(int i=2;i<=n;i++){
-            arr[i]=arr[i-1]+arr[i-2];
+        // Create a DP array of size n+1 to store the number of ways to reach each step
+        vector<int> arr(n + 1);
+
+        // Base cases:
+        // There is 1 way to stay at step 0 (do nothing)
+        // There is 1 way to reach step 1 (one single step)
+        arr[0] = 1;
+        arr[1] = 1;
+
+        // Build the DP array from step 2 to n
+        // Each step can be reached either from the previous step (i-1)
+        // or from two steps below (i-2)
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
+
+        // The answer is the number of ways to reach the nth step
         return arr[n];
     }
 };
