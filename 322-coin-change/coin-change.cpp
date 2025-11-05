d5b426d031855cfac092ad9f3e84ce01ac1538e2
Kavya0224 <kavyasanger448@gmail.com>
2025-11-05 20:59:00 +0530

    Time: 49 ms (28.03%) | Memory: 53.1 MB (12.82%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,51 +1,52 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // dp[i] will store the minimum number of coins needed to make up amount i
-        // Initialize with a large number (amount + 1 acts as "infinity")
-        vector<int> dp(amount + 1, amount + 1);
+        int n = coins.size();
+        // Create DP table where t[i][j] represents the minimum number of coins 
+        // needed to make amount 'j' using the first 'i' coins.
+        vector<vector<int>> t(n + 1, vector<int>(amount + 1, 0));
 
-        // Base case: to make amount 0, we need 0 coins
-        dp[0] = 0;
-
-        // Iterate over all amounts from 1 to the target amount
-        for (int i = 1; i <= amount; ++i) {
-            // Try every coin denomination
-            for (int coin : coins) {
-                // If the current coin can be used (i.e., i - coin is not negative)
-                if (i - coin >= 0) {
-                    // Choose the minimum between current dp[i] 
-                    // and using one coin + best result for (i - coin)
-                    dp[i] = min(dp[i], 1 + dp[i - coin]);
-                }
+        // ---------- Initialization ----------
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (i == 0)
+                    // If there are 0 coins, we cannot make any positive amount.
+                    // Use INT_MAX - 1 to avoid integer overflow during addition.
+                    t[i][j] = INT_MAX - 1;
+                else if (j == 0)
+                    // If amount is 0, we need 0 coins.
+                    t[i][j] = 0;
             }
         }
 
-        // If dp[amount] was never updated, return -1 (no valid combination)
-        // Otherwise, return the minimum number of coins found
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
+        // ---------- Base Case for First Coin ----------
+        // If we only have one coin type, we can form amounts that are multiples of it.
+        for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                // If 'j' is divisible by coins[0], we can make the amount using (j / coins[0]) coins.
+                t[1][j] = j / coins[0];
+            else
+                // Otherwise, it's not possible to form that amount using only this coin.
+                t[1][j] = INT_MAX - 1;
+        }
+
+        // ---------- Build DP Table (Unbounded Knapsack Logic) ----------
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (coins[i - 1] <= j)
+                    // Option 1: Exclude the coin (t[i-1][j])
+                    // Option 2: Include the coin (1 + t[i][j - coins[i-1]])
+                    // Use 't[i][...]' for the include case since coins can be reused.
+                    t[i][j] = min(t[i - 1][j], t[i][j - coins[i - 1]] + 1);
+                else
+                    // If coin value is greater than amount, we cannot include it.
+                    t[i][j] = t[i - 1][j];
+            }
+        }
+
+        // ---------- Result ----------
+        int res = t[n][amount];
+        // If the result is still "infinite", return -1 (no combination possible)
+        return (res == INT_MAX - 1) ? -1 : res;
     }
 };
-
-// class Solution {
-// public:
-//     int coinChange(vector<int>& coins, int amount) {
-//         sort(coins.begin(),coins.end());
-//         int n=coins.size()-1;
-//         int ct=0;
-//         int i=n;
-//         while(i>=0){
-//             if(amount==0){
-//                 return ct;
-//             }
-//             if(amount>=coins[i]){
-//                 ct++;
-//                 amount-=coins[i];
-//             }
-//             else{
-//                 i--;
-//             }
-//         }
-//         return -1;
-//     }
-// };
