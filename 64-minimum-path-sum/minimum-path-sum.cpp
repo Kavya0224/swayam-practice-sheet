304b6f0c31873b730621eb85312b225bb7376174
Kavya0224 <kavyasanger448@gmail.com>
2025-11-05 16:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (98.25%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
-                if(i==0){
-                    grid[i][j]+=grid[i][j-1];
+        int m = grid.size();        // Number of rows
+        int n = grid[0].size();     // Number of columns
+
+        // Traverse each cell in the grid
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // Skip the starting point (top-left corner)
+                if (i == 0 && j == 0) continue;
+
+                // If in the first row, can only come from the left
+                if (i == 0) {
+                    grid[i][j] += grid[i][j - 1];
                 }
-                else if(j==0){
-                    grid[i][j]+=grid[i-1][j];
+                // If in the first column, can only come from above
+                else if (j == 0) {
+                    grid[i][j] += grid[i - 1][j];
                 }
-                else{
-                    grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
+                // Otherwise, choose the smaller path (top or left)
+                else {
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
                 }
             }
-        }         
-        return grid[m-1][n-1];
+        }
+
+        // The bottom-right cell now contains the minimum path sum
+        return grid[m - 1][n - 1];
     }
 };
