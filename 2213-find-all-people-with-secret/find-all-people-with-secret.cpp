8666058c0e5795ad2a139b8cd50a2f650abe7a22
Kavya0224 <kavyasanger448@gmail.com>
2025-12-19 11:09:44 +0530

    Time: 177 ms (60.43%) | Memory: 199.5 MB (42.82%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.cpp
@@ -1,65 +1,51 @@
 class Solution {
 public:
+    int find(int x, vector<int>& p) {
+        return p[x] == x ? x : p[x] = find(p[x], p);
+    }
+
+    void unite(int a, int b, vector<int>& p) {
+        int pa = find(a, p), pb = find(b, p);
+        if (pa != pb) p[pb] = pa;
+    }
+
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-
         sort(meetings.begin(), meetings.end(),
-             [](auto &a, auto &b) {
-                 return a[2] < b[2];
-             });
+             [](auto& a, auto& b) { return a[2] < b[2]; });
 
-        vector<int> parent(n);
+        vector<int> parent(n), know(n, 0);
         iota(parent.begin(), parent.end(), 0);
 
-        function<int(int)> find = [&](int x) {
-            if(parent[x] != x)
-                parent[x] = find(parent[x]);
-            return parent[x];
-        };
+        know[0] = know[firstPerson] = 1;
 
-        auto unite = [&](int a, int b) {
-            parent[find(a)] = find(b);
-        };
-
-        vector<int> know(n, 0);
-        know[0] = 1;
-        know[firstPerson] = 1;
-
-        for(int i = 0; i < meetings.size(); ) {
-            int time = meetings[i][2];
-            vector<pair<int,int>> batch;
+        for (int i = 0; i < meetings.size(); ) {
+            int t = meetings[i][2];
+            vector<int> temp;
 
             int j = i;
-            while(j < meetings.size() && meetings[j][2] == time) {
-                unite(meetings[j][0], meetings[j][1]);
-                batch.push_back({meetings[j][0], meetings[j][1]});
+            while (j < meetings.size() && meetings[j][2] == t) {
+                unite(meetings[j][0], meetings[j][1], parent);
+                temp.push_back(meetings[j][0]);
+                temp.push_back(meetings[j][1]);
                 j++;
             }
 
-            unordered_set<int> informed;
-            for(auto &p : batch) {
-                if(know[p.first] || know[p.second]) {
-                    informed.insert(find(p.first));
-                    informed.insert(find(p.second));
-                }
-            }
+            for (int x : temp)
+                if (know[x])
+                    know[find(x, parent)] = 1;
 
-            for(auto &p : batch) {
-                if(informed.count(find(p.first)) ||
-                   informed.count(find(p.second))) {
-                    know[p.first] = know[p.second] = 1;
-                } else {
-                    parent[p.first] = p.first;
-                    parent[p.second] = p.second;
-                }
-            }
+            for (int x : temp)
+                know[x] |= know[find(x, parent)];
+
+            for (int x : temp)
+                parent[x] = x;
 
             i = j;
         }
 
-        vector<int> ans;
-        for(int i = 0; i < n; i++)
-            if(know[i]) ans.push_back(i);
-
-        return ans;
+        vector<int> res;
+        for (int i = 0; i < n; i++)
+            if (know[i]) res.push_back(i);
+        return res;
     }
 };
