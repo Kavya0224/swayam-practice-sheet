f43f6aa5289e944fa4a2b922957961f3d4990fc1
Kavya0224 <kavyasanger448@gmail.com>
2025-12-20 15:07:09 +0530

    Time: 341 ms (42.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT name
+from Employee
+where id in(
+    SELECT managerId
+    from Employee
+    group by managerId
+    having count(managerId)>=5
+);
