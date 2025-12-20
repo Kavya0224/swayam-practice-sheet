fb8a811160f366f3b399e6daf4567931ae76ba4c
Kavya0224 <kavyasanger448@gmail.com>
2025-12-20 13:12:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (28.75%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> t(n + 1, 0); // t[i] = min cost to reach top from step i
+        vector<int> t(n + 2, 0); // t[i] = min cost to reach top from step i
 
         // Base case: cost to reach top from top = 0
         t[n] = 0;
@@ -10,7 +10,7 @@ public:
         // Fill DP table from the end
         for (int i = n - 1; i >= 0; i--) {
             int oneStep = cost[i] + t[i + 1];
-            int twoStep = (i + 2 <= n) ? cost[i] + t[i + 2] : cost[i]; // avoid out-of-bounds
+            int twoStep = cost[i] + t[i + 2] ; // avoid out-of-bounds
             t[i] = min(oneStep, twoStep);
         }
 
