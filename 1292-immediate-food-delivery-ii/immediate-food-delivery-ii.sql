8c5becb1eae1cb299829e279e522a64a089b33d9
Kavya0224 <kavyasanger448@gmail.com>
2025-12-23 15:00:55 +0530

    Time: 567 ms (78.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,11 @@
+SELECT 
+    ROUND(
+        100 * AVG(order_date = customer_pref_delivery_date),
+        2
+    ) AS immediate_percentage
+FROM delivery
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
+    FROM delivery
+    GROUP BY customer_id
+);
