40bcff07cfdeb6912e34fb05fe47a2e95b691586
Kavya0224 <kavyasanger448@gmail.com>
2025-12-25 20:42:04 +0530

    Time: 91 ms (34.95%) | Memory: 75.8 MB (74.08%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        // Create a reversed copy of the input string
-        string rev = s;
-        int n = s.length();
-        reverse(rev.begin(), rev.end());
-        
-        // dp[i][j] will store the length of the longest common subsequence
-        // between s[0..i-1] and rev[0..j-1]
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        
-        // Build the DP table
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                
-                // If characters match, add 1 to the result of the previous substring
-                if (s[i - 1] == rev[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } 
-                // Otherwise, take the maximum of ignoring one character from either string
-                else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        string rev=s;
+        int n=s.length();
+        reverse(rev.begin(),rev.end());
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==rev[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        
-        // The bottom-right cell contains the length of the longest palindromic subsequence
         return dp[n][n];
+
     }
 };
