0c98fd45b1accee386c29ae8989a5d53226d3208
Kavya0224 <kavyasanger448@gmail.com>
2026-01-10 13:21:11 +0530

    Time: 23 ms (80.79%) | Memory: 27.3 MB (76.81%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        // dp[i] will store the length of the longest common subsequence (LCS)
-        // that ends at text1[i] for the portion of text2 processed so far.
-        vector<int> dp(text1.length(), 0);
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
 
-        // Tracks the overall longest LCS length found
-        int longest = 0;
-
-        // Iterate through each character in text2
-        for (char c : text2) {
-            int curLength = 0; // Holds the maximum LCS length up to the current point in text1
-
-            // Compare current character from text2 with all characters of text1
-            for (int i = 0; i < dp.size(); i++) {
-                // If we find a longer LCS so far, update curLength
-                if (curLength < dp[i]) {
-                    curLength = dp[i];
                 }
-                // If characters match, we can extend the previous subsequence by 1
-                else if (c == text1[i]) {
-                    dp[i] = curLength + 1;           // Update dp[i] with new LCS length
-                    longest = max(longest, dp[i]);   // Track the global maximum
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-
-        // Return the length of the longest common subsequence found
-        return longest;        
+        return dp[n][m];
     }
 };
