f21a6fedbff6a820680163bd0fb239edbe9786a9
Kavya0224 <kavyasanger448@gmail.com>
2026-01-10 14:24:34 +0530

    Time: 37 ms (28.40%) | Memory: 31.6 MB (15.47%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int minInsertions(string s) {
+        // Step 1: Create a reversed copy of the string
+        // This will help us compute the Longest Palindromic Subsequence (LPS)
         string rev = s;
         int n = s.length();
         reverse(rev.begin(), rev.end());
         
-        // dp[i][j] will store the length of the longest common subsequence
-        // between s[0..i-1] and rev[0..j-1]
+        // Step 2: Create a DP table for LCS (Longest Common Subsequence)
+        // dp[i][j] = length of LCS between s[0..i-1] and rev[0..j-1]
         vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
         
-        // Build the DP table
+        // Step 3: Build the DP table
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 
-                // If characters match, add 1 to the result of the previous substring
+                // If characters match, extend the previous LCS by 1
                 if (s[i - 1] == rev[j - 1]) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 } 
-                // Otherwise, take the maximum of ignoring one character from either string
+                // If they don't match, take the maximum LCS by ignoring
+                // either the current character from s or rev
                 else {
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
         
-        // The bottom-right cell contains the length of the longest palindromic subsequence
-        return n-dp[n][n];
+        // Step 4: The length of the longest palindromic subsequence is dp[n][n]
+        // Minimum insertions required = total length - length of LPS
+        return n - dp[n][n];
     }
 };
