e1419befe1f2b3781185f45c25387fdf3c5adc42
Kavya0224 <kavyasanger448@gmail.com>
2026-01-16 12:15:20 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (34.28%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]) return nums[i];
+        unordered_set<int>s;
+        for(int x:nums)
+        {
+            if(s.count(x)) //set me unique elements hi store hote hai so is line me check krenge element ek se jada baar hai agar hai to wahi answer hai 
+            {
+                return x; //duplicate
+            }
+            s.insert(x); // and uske alawa baki elements ko insert krenge 
         }
         return -1;
     }
